c4707b3be05d4363cd404beaeb34dbc6d2e248d5
giobra <gioobrandino@gmail.com>
2025-11-24 00:50:49 -0300

    servidor implementado

--- servidor.py
+++ servidor.py
@@ -23,5 +23,70 @@ def calculaHashMD5(nome_arquivo):
     return h.hexdigest()
 
 def lidarCliente (conexao, endereco):
+    print(f"[NOVA CONEXÃO] {endereco} conectado.")
+    conexao_ativa = True
+    try:
+        while conexao_ativa:
+            comando_raw = conexao.recv(BUFFER_SIZE).decode('utf-8')
+
+            if not comando_raw:
+                conexao_ativa = False
+            else:
+                partes_comando = comando_raw.split()
+                comando = partes_comando[0].upper()
+
+                print(f"[{endereco}] Comando recebido: {comando_raw}")
+
+                if comando == "ARQUIVO" and len(partes_comando) > 1:
+                    nome_arquivo = partes_comando[1]
+                    
+                    if os.path.exists(nome_arquivo):
+                        tamanho_arquivo = os.path.getsize(nome_arquivo)
+                        hash_arquivo = calculaHashMD5(nome_arquivo)
+
+                        conexao.sendall(f"OK {tamanho_arquivo} {hash_arquivo}".encode('utf-8'))
+                        with open (nome_arquivo, 'rb') as f:
+                            bytes_lidos = f.read(BUFFER_SIZE)
+                            while bytes_lidos:
+                                conexao.sendall(bytes_lidos)
+                                bytes_lidos = f.read(BUFFER_SIZE)
+                        print(f"[{endereco}] Arquivo '{nome_arquivo}' enviado com sucesso.")
+                    else:
+                        conexao.sendall("ERRO Arquivo não encontrado.".encode("utf-8"))
+                        print(f"[{endereco}] Tentativa de acesso a arquivo inexistente: {nome_arquivo}")
+                elif comando == "CHAT":
+                    conexao.sendall("OK CHAT".encode('utf-8'))
+                    chat_ativo =  True
+                    while chat_ativo:
+                        mensagem_cliente = conexao.recv(BUFFER_SIZE).decode('utf-8')
+                        if mensagem_cliente and mensagem_cliente.upper() != 'SAIR':
+                            print(f"[{endereco} - CHAT] Mensagem: {mensagem_cliente}")
+                            conexao.sendall(f"{mensagem_cliente}".encode('utf-8'))
+                        else:
+                            chat_ativo = False
+                            print(f"[{endereco}] Fim do modo CHAT.")
+                elif comando == "SAIR":
+                    print(f"[{endereco}] Cliente solicitou desconexão.")
+                    conexao_ativa = False
+    except ConnectionResetError:
+        print(f"[AVISO] Conexão com {endereco} foi perdida inesperadamente.")
+    except Exception as e:
+        print(f"[ERRO] Ocorreu um erro com o cliente {endereco}: {e}")
+    finally:
+        print(f"[CONEXÃO FECHADA] Conexão com {endereco} encerrada.")
+        conexao.close()
 
 def iniciarServidor():
+    servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    servidor.bind((HOST, PORT))
+    servidor.listen(5)
+    print(f"[*] Servidor TCP escutando em {HOST}:{PORT}")
+
+    while True:
+        conexao, endereco = servidor.accept()
+        thread_cliente = threading.Thread(target=lidarCliente, args=(conexao, endereco))
+        thread_cliente.start()
+        print(f"[CONEXÕES ATIVAS] {threading.active_count() - 1}")
+
+if __name__ == "__main__":
+    iniciarServidor()
